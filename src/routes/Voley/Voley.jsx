ce6084d37ba04cacb7a8d66036e938fb1db19a23
maxi <maxi_cornalba@hotmail.com.ar>
2024-02-22 17:53:59 -0300

    amost finish voley

--- src/routes/Voley/Voley.jsx
+++ src/routes/Voley/Voley.jsx
@@ -3,24 +3,34 @@ import NavBrowser from '../../components/NavBrowser';
 import Footer from '../../components/Footer';
 import bannerVoley from "../../assets/banner-voley.jpeg";
 import imgVoley from "../../assets/imgVoley.png";
+import voleyPic from "../../assets/voleyPic.jpg";
 import './Voley.css';
 
 export default function Voley() {
   return (
-    <div>
+    <div className="container-div-voley">
       <NavBrowser />
-      <div className="container-voley">
-        <div className="banner-container">
-          <img className="bannerVoley" src={bannerVoley} alt="banner actividades" />
-          <div className='container-text-voley'>
-            <div className="text-container">
-              <h2 className="title">¡Clases semanales! Más de 140 inscriptos.</h2>
-              <p className="subtitle">
-                Juveniles y mayores de 18 años. ¡Súmate a nuestra familia!
-              </p>
-            </div>
-          </div>
-          <img className="imageVoley" src={imgVoley} alt="voley player" />
+      <div className="container-banner-voley">
+        <img className="banner-voley" src={bannerVoley} alt="banner voley" />
+        <img className="player-image" src={imgVoley} alt="image player" />
+        <div className="container-banner-text-voley">
+          <h2 className="title">¡Clases semanales! Más de 140 inscriptos.</h2>
+          <p className="subtitle">
+            Juveniles y mayores de 18 años. ¡Súmate a nuestra familia!
+          </p>
+        </div>
+      </div>
+      <div className="container-voleypic-family">
+        <img className="pic-voley1" src={voleyPic} alt="imagen equipo de voley" />
+        <div className="container-text-voley">
+          <p>Las clases se desarrollan tanto en las instalaciones de nuestra Pista en el Club como en el Nexo Polideportivo Súmate a nuestra familia. Se vienen lindas experiencias para nuestros Jugadores/as, queremos que seas parte de todo esto.
+          </p>
+          <p>Profesor juveniles: Andy Javier schwindt</p>
+          <p>Profesor mayores de 18: Ruben Alberto Abelenda</p>
+          <p>Info al: 1131848212 y/o 1150375322 (Comité Deportivo)</p>
+          <p>Mándanos un Whatsapp a los números aportados para informarte de los cronograma de prácticas y todo lo que quieras saber. Te esperamos
+          </p>
+
         </div>
       </div>
       <Footer />
