45157d103b2aaf5d06d9703004eb81a6755ec24a
maxi-cor <ecornalba@estudiolevex.com.ar>
2024-02-23 11:19:04 -0300

    change info in text in hockey

--- src/routes/Hockey/Hockey.jsx
+++ src/routes/Hockey/Hockey.jsx
@@ -14,7 +14,7 @@ export default function Hockey() {
         <img className="banner-hockey" src={bannerVoley} alt="banner hockey"/>
         <img className="player-image-hockey" src={hockeyPic} alt="player" />
         <div className="container-banner-text-hockey">
-          <h2 className="title">Anótate en Hockey Mariano Acosta</h2>
+          <h2 className="title">Anótate en Hockey CLUB M.ACOSTA</h2>
           <p className="subtitle">
           ¡Descubre la emoción del hockey y únete a nuestra comunidad deportiva hoy mismo!
           </p>
@@ -23,10 +23,9 @@ export default function Hockey() {
       <div className="container-hockeypic-family">
         <img className="pic-hockey1" src={hockey} alt="equipo de voley" />
         <div className="container-text-hockey">
-          <p>Las clases se desarrollan tanto en las instalaciones de nuestra Pista en el Club como en el Nexo Polideportivo Súmate a nuestra familia. Se vienen lindas experiencias para nuestros Jugadores/as, queremos que seas parte de todo esto.
+          <p>Una Actividad en desarrollo en la cual nuestra institución APUESTA MUCHISIMO, sabemos que la variedad en el Deporte es fundamental, por eso te ofrecemos que te sumes a Nuestro Hockey Club. Mariano Acosta. Grupos Infantiles Juveniles y Mayores discriminadas por Categorías, tres clases semanales, apuntamos a ingresar un nuevo torneo 2024, acompáñanos en esta nueva etapa.
           </p>
-          <p>Profesor juveniles: Andy Javier schwindt</p>
-          <p>Profesor mayores de 18: Ruben Alberto Abelenda</p>
+          <p>Profesores: Aylen Milagros Aquino y Ruben Alberto Abelenda </p>
           <p>Info al: 1131848212 y/o 1150375322 (Comité Deportivo)</p>
           <p>Mándanos un Whatsapp a los números aportados para informarte de los cronograma de prácticas y todo lo que quieras saber. Te esperamos
           </p>
