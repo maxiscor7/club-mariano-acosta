5bcab918563b9c16bb0a45386ebbaf5557dac523
maxi-cor <ecornalba@estudiolevex.com.ar>
2024-05-17 11:51:19 -0300

    changes in text boutique

--- src/routes/Indumentaria/Indumentaria.jsx
+++ src/routes/Indumentaria/Indumentaria.jsx
@@ -47,14 +47,14 @@ export default function Indumentaria() {
       <div className='container-descriptionAndimage-boutique'>
         <img src={shopingBag} alt='bag-png' />
         <div className='container-description-boutique'>
-          <p>Conoce nuestra BOUTIQUE DEL DEPORTE “Lugar donde exponemos nuestra Novedades”, acércate y adquirí tu camiseta o conjunto deportivo OFICIAL del C.S.D.M.A. Aca tenemos lo que necesitas para identificarte como miembro de nuestra institución.</p>
+          <p>Conoce nuestra <b>BOUTIQUE DEL DEPORTE</b> “Lugar donde exponemos nuestra novedades”, acércate y adquirí tu camiseta o conjunto deportivo <b>OFICIAL</b> del <b>C.S.D.M.A.</b> Aca tenemos lo que necesitas para identificarte como miembro de nuestra institución.</p>
           <ul>
-            <li>Conjunto OFICIAL camiseta y Short celeste y Blanco que se utiliza para los eventos deportivos competencias o amistosos.</li>
-            <li>Conjunto de entrenamiento, camiseta y Short de color oscuro.</li>
-            <li>Conjunto Deportivo, campera y pantalón largo totalmente adaptable para la actividad física.</li>
+            <li>Conjunto <b>OFICIAL</b> camiseta y short celeste y blanco que se utiliza para los eventos deportivos competencias o amistosos.</li>
+            <li>Conjunto de entrenamiento, camiseta y short de color oscuro.</li>
+            <li>Conjunto deportivo, campera y pantalón largo totalmente adaptable para la actividad física.</li>
             <li>Medias, accesorios y mucho mas.</li>
           </ul>
-          <p>Acércate a nuestro Local y Observa lo que tenemos para vos. </p>
+          <p>Acércate a nuestro local y observa lo que tenemos para vos. </p>
           <div className='container-navlink-boutique'>
             <div className='navlinkBoutique'>
               <NavLink style={{textDecoration: 'none', color: 'white'}} aria-current="page" to={"/contacto"}>¡Te esperamos!</NavLink>
